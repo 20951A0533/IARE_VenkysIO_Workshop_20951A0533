23a23b811f247772b9ff051b4dc233661e37aa7c
20951A0533 <119832056+20951A0533@users.noreply.github.com>
2023-02-25 11:24:37 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,7 @@
-
+y=int(input("enter the cost taken to buy toys:"))
+x=int(input("enter no of toys bought:"))
+z=int(input("enter the amount left:"))
+cost=y/x
+print("cost for one toy:",cost)
+a=y+z
+print("cost had:",a)
